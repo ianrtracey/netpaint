ce0c4b8fd0b72f208582770f563f9658db45633a
Ian Tracey <ianrtracey@gmail.com>
2015-10-20 11:14:24 -0700

    Adds canvas listeners

--- src/view/Canvas.java
+++ src/view/Canvas.java
@@ -2,12 +2,18 @@ package view;
 
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import javax.swing.JPanel;
 
-public class Canvas extends JPanel {
+public class Canvas extends JPanel implements ActionListener, MouseListener {
 	
 	private void draw(Graphics g) {
+		
+		ArrayList<PaintObject> paintObject = new ArrayList<PaintObject>();
 		
 		Graphics2D g2d = (Graphics2D) g;
 		
@@ -20,6 +26,42 @@ public class Canvas extends JPanel {
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		draw(g);
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 	
 
